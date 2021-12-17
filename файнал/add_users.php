29ebddeea59cc3fdf4af62f5a33ef83c5f7f63d4
Albinakaramatdinnn <91120226+Albinakaramatdinnn@users.noreply.github.com>
2021-12-17 14:00:19 +0600

    Delete файнал directory

--- файнал/add_users.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "server.php";
-$username = $_POST['username'];
-$email = $_POST['email'];
-$password_unencrypted = $_POST['password'];
-$password=md5($password_unencrypted);
-mysqli_query($db, "INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES (NULL, '$username', '$email', '$password')");
-header('Location: adminController.php');
